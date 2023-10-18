8108a2be50cae2aa8716c67e88b7a4f1e3ce0be2
andrey_turchin <tur4intwo@gmail.com>
2023-10-18 17:17:25 +0300

    add action-creators deleteBook

--- frontend/src/components/bookList/BookList.js
+++ frontend/src/components/bookList/BookList.js
@@ -1,10 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteBook } from '../../redux/books/actionCreators';
 
 import './BookList.css';
 
 const BookList = () => {
 
 	const books = useSelector((state) => state.books);
+	const dispatch = useDispatch();
+
+	const handleDeleteBook = (id) => {
+		dispatch(deleteBook(id));
+	}
 
 	return (
 		<div className="app-block book-list">
@@ -17,6 +23,12 @@ const BookList = () => {
 						<li key={book.id}>
 							<div className="book-info">
 								{++i}. {book.title} by <strong>{book.author}</strong>
+							</div>
+							<div className="book-actions">
+								<button
+									onClick={() => handleDeleteBook(book.id)}>
+									delete
+								</button>
 							</div>
 						</li>
 					))}
