e6105b8c932562686a7c132e01d09dcfa7e545e8
andrey_turchin <tur4intwo@gmail.com>
2023-10-18 16:13:35 +0300

    add redux and store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,18 @@
 import React, { StrictMode } from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+
+import store from "./redux/store";
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</StrictMode>
 );
 
