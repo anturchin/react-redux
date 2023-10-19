6e215c4097b34d762944e0599d6109858c76da3a
andrey_turchin <tur4intwo@gmail.com>
2023-10-19 21:19:33 +0300

    added create asyncthunk

--- frontend/src/components/bookForm/BookForm.js
+++ frontend/src/components/bookForm/BookForm.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 // import { addBook } from '../../redux/books/actionCreators';
-import { addBook, thunkFunction } from '../../redux/slices/booksSlice';
+import { addBook, fetchBook } from '../../redux/slices/booksSlice';
 import booksData from '../../data/books.json';
 
 import createBookWithID from '../../utils/createBookWithId';
@@ -36,7 +36,7 @@ const BookForm = () => {
 	}
 
 	const handleAddRandomViaAPI = () => {
-		dispatch(thunkFunction)
+		dispatch(fetchBook())
 	}
 
 	return (
--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,9 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 import createBookWithID from '../../utils/createBookWithId';
 
 const initialState = [];
+
+export const fetchBook = createAsyncThunk(
+	'books/fetchBook',
+	async () => {
+		const res = await axios.get('http://localhost:5000/random-book');
+		return res.data;
+	}
+)
 
 const bookSlice = createSlice({
 	name: 'books',
@@ -22,23 +30,30 @@ const bookSlice = createSlice({
 				}
 			})
 		},
+	},
+	extraReducers: (builder) => {
+		builder.addCase(fetchBook.fulfilled, (state, action) => {
+			if (action.payload.title && action.payload.author) {
+				state.push(createBookWithID(action.payload, 'API'));
+			}
+		});
 	}
 })
 
 
 export const { addBook, deleteBook, toggleFavoriteBook } = bookSlice.actions;
 
-export const thunkFunction = async (dispatch, getState) => {
+// export const thunkFunction = async (dispatch, getState) => {
 
-	try {
-		const res = await axios.get('http://localhost:5000/random-book');
-		if (res?.data?.title && res?.data?.author) {
-			dispatch(addBook(createBookWithID(res.data, 'API')));
-		}
-	} catch (error) {
-		console.log(error);
-	}
-}
+// 	try {
+// 		const res = await axios.get('http://localhost:5000/random-book');
+// 		if (res?.data?.title && res?.data?.author) {
+// 			dispatch(addBook(createBookWithID(res.data, 'API')));
+// 		}
+// 	} catch (error) {
+// 		console.log(error);
+// 	}
+// }
 
 export const selectBooks = (state) => state.books;
 
